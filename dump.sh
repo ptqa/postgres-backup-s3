69019fb7563b2381a73230c1ff581e75dc5d262a
Tony Nyurkin <ptqa@users.noreply.github.com>
2019-07-26 14:23:10 +0300

    Allow connection params overwrite with ENV

--- dump.sh
+++ dump.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-pg_dump -h postgres -U postgres postgres > /tmp/dump.db; /usr/local/bin/aws s3 cp /tmp/dump.db $PG_DUMP_LOCATION  > /dev/null
+pg_dump -h $PG_HOST -U $PG_USER $PG_DB > /tmp/dump.db; /usr/local/bin/aws s3 cp /tmp/dump.db $PG_DUMP_LOCATION  > /dev/null

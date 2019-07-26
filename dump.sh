09f6dd3be8afdb29d23f006f425dbd53ca69fdab
Tony Nyurkin <ptqa@users.noreply.github.com>
2019-07-26 16:28:48 +0300

    allow additional options for pg_dump

--- dump.sh
+++ dump.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-pg_dump -h $PG_HOST -U $PG_USER $PG_DB > /tmp/dump.db; /usr/local/bin/aws s3 cp /tmp/dump.db $PG_DUMP_LOCATION  > /dev/null; rm /tmp/dump.db
+pg_dump $PG_DUMP_OPTIONS -h $PG_HOST -U $PG_USER $PG_DB > /tmp/dump.db; /usr/local/bin/aws s3 cp /tmp/dump.db $PG_DUMP_LOCATION  > /dev/null; rm /tmp/dump.db

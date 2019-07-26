ff13b87ecbc1203b8befd4efbc4b7d51aaa32cdc
Tony Nyurkin <ptqa@users.noreply.github.com>
2019-07-26 17:19:05 +0300

    try streaming for s3

--- dump.sh
+++ dump.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-pg_dump $PG_DUMP_OPTIONS -h $PG_HOST -U $PG_USER $PG_DB > /tmp/dump.db; /usr/local/bin/aws s3 cp /tmp/dump.db $PG_DUMP_LOCATION  > /dev/null; rm /tmp/dump.db
+pg_dump $PG_DUMP_OPTIONS -h $PG_HOST -U $PG_USER $PG_DB | /usr/local/bin/aws s3 cp - $PG_DUMP_LOCATION  > /dev/null
